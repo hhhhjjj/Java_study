432c4dcbbdea5e4f944775be9461107a4309857f
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2020-01-28 09:35:16 +0800

    1

---
+++ src/study/Text.java
@@ -0,0 +1,14 @@
+package study;
+//声明包名
+
+public class Text {
+//	新建类
+	
+//	创建类和main方法
+//	java包里面只能有一个public主类，程序会自动找到主类下的main方法
+	public static void main(String[] args) {
+
+		System.out.println("hello world!");
+	}
+
+}
